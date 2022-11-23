b3119e06a7a1b9548ea339e51adbaeea26278f5e
Cristiano Silva <114546598+CristianoSilva22@users.noreply.github.com>
2022-11-23 15:48:28 -0300

    Update and rename main_screen.py to main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import os
 c=5
+l=[]
+try:
+    mt=open("database.txt","r+")
+    for i in mt:
+        l.append(i.replace("\n",""))
+    mt.close()
+    os.remove("database.txt")
+except:
+    print("Primeira Inicialização concluída!\n")
 print("Bem vindo a crissystems - To do List")
 while(c!=0):
     print("\nDigite 1 para adicionar uma tarefa\nDigite 2 para listar as tarefas\nDigite 3 para editar alguma tarefa\nDigite 4 para excluir uma tarefa\nDigite 0 para sair")
@@ -19,7 +29,12 @@ while(c!=0):
         l[i]=input("Digite a tarefa com a alteração\n")
         print("Tarefa alterada!")
     elif(c==4):
-        i=input("\nDigite a tarefa a ser alterada\n")
+        i=input("\nDigite a tarefa a ser excluída\n")
         i=l.index(i)
         del(l[i])
         print("Tarefa excluída!")
+mt=open("database.txt","a")
+for i in l:
+    mt.write(i)
+    mt.write("\n")
+mt.close()
