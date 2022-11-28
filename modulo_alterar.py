ba7a66fbb1f8f97343246f3ea2b805c67f3e4e10
Cristiano Silva <114546598+CristianoSilva22@users.noreply.github.com>
2022-11-28 15:07:47 -0300

    Add files via upload

---
+++ modulo_alterar.py
@@ -0,0 +1,10 @@
+def alterar_elemento(lista):
+    armazenador=input("\nDigite a tarefa a ser alterada\n")
+    try:
+        armazenador=lista.index(armazenador)
+        lista[armazenador]=input("Digite a tarefa com a alteracao\n")
+        print("Tarefa alterada!")
+        return lista
+    except:
+        print("Tarefa inexistente")
+        return lista
---
+++ modulo_excluir.py
@@ -0,0 +1,10 @@
+def excluir_elemento(lista):
+    armazenador=input("\nDigite a tarefa a ser excluida\n")
+    try:
+        armazenador=lista.index(armazenador)
+        del(lista[armazenador])
+        print("Tarefa excluida!")
+        return lista
+    except:
+        print("Tarefa inexistente")
+        return lista
