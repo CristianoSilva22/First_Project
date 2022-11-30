624b739552b012ee1f787864d9694c87be712f03
Cristiano Silva <114546598+CristianoSilva22@users.noreply.github.com>
2022-11-29 23:10:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-import os, modulo_adicionar, modulo_listar, modulo_alterar, modulo_excluir
-c=5
-l=[]
-try:
-    mt=open("database.txt","r+")
-    for i in mt:
-        l.append(i.replace("\n",""))
-    mt.close()
-    os.remove("database.txt")
-except:
-    print("Primeira Inicialização concluída!\n")
+import modulo_adicionar, modulo_listar, modulo_alterar, modulo_excluir, modulo_bd
+indicador_funcao=5
+database=modulo_bd.iniciar_bd()
 print("Bem vindo a crissystems - To do List")
-while(c!=0):
+while(indicador_funcao!=0):
     print("\nDigite 1 para adicionar uma tarefa\nDigite 2 para listar as tarefas\nDigite 3 para editar alguma tarefa\nDigite 4 para excluir uma tarefa\nDigite 0 para sair")
-    c=int(input())
-    if(c==0):
+    indicador_funcao=int(input())
+    if(indicador_funcao==0):
         print("Até a próxima!")
-    elif(c==1):
-        l=modulo_adicionar.adicionar_elemento(l)
-    elif(c==2):
-        modulo_listar.listar_elementos(l)
-    elif(c==3):
-        l=modulo_alterar.alterar_elemento(l)
-    elif(c==4):
-        l=modulo_excluir.excluir_elemento(l)
-mt=open("database.txt","a")
-for i in l:
-    mt.write(i)
-    mt.write("\n")
-mt.close()
+    elif(indicador_funcao==1):
+        database=modulo_adicionar.adicionar_elemento(database)
+    elif(indicador_funcao==2):
+        modulo_listar.listar_elementos(database)
+    elif(indicador_funcao==3):
+        database=modulo_alterar.alterar_elemento(database)
+    elif(indicador_funcao==4):
+        database=modulo_excluir.excluir_elemento(database)
+    else:
+        print("Digite uma operacao valida!")
+modulo_bd.encerrar_bd(database)
+
